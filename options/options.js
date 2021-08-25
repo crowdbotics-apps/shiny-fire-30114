16e1fa315709fb5a33e4f928b27ace101a2c8e30
Crowdbotics <team@crowdbotics.com>
2021-08-25 23:14:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "shiny_fire_30114Identifier",
+  url: "https://shiny_fire_30114Identifier.botics.co",
+  api: "https://shiny_fire_30114Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
